399a782ffd7fa87cb5af889b204660cebde89fc8
Capital City FC <ccfootbalclub@gmail.com>
2025-08-09 20:59:07 +0000

    chnage the icon in the sibear to a placeholder

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,9 +12,7 @@ export const Icons = {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-      <path d="M15.5 8.5c-.7-1-2-1.5-3.5-1.5-2.5 0-5 1.5-5 5" />
-      <path d="M15.5 15.5c-.7 1-2 1.5-3.5 1.5-2.5 0-5-1.5-5-5" />
+      <rect width="18" height="18" x="3" y="3" rx="2" />
     </svg>
   ),
 };
